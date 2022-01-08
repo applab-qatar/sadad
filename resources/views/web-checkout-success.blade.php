7db9f1036d2f234fe1700fd89b8f9b46dd256beb
Qasim Nouh <vs.dev50@gmail.com>
2022-01-08 16:17:43 +0200

    [ADD] add success view and translations

--- resources/views/web-checkout-success.blade.php
+++ resources/views/web-checkout-success.blade.php
@@ -7,20 +7,11 @@
     <div class="row">
         <div class="col-md-12">
             <div id="invoice" style="text-align:center">
-                <div class="invoice-wrapper not-successful">
+                <div class="invoice-wrapper successful">
                     <div class="content-wrapper"
                          style="padding:20px 20px 10px;border-radius: 30px;margin:20px 0;background-color: #f8f8ff">
                         <div class="confirm fail text-center">
-                            <h3 style="color:#FF6262">@lang('global.payment_failed')!</h3>
-                            @if ($errors->any())
-                                <div class="alert alert-danger">
-                                    <ul>
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            @endif
+                            <h3 style="color:#53ff53">@lang('global.payment_success')!</h3>
                         </div>
                     </div>
                 </div>
