ea4afac984b8128e1fde8449283fa6d503c2e902
Maanu <manu@applab.qa>
2022-01-08 17:35:24 +0300

    payment finish page

--- resources/views/web-checkout-success.blade.php
+++ resources/views/web-checkout-success.blade.php
@@ -11,16 +11,8 @@
                     <div class="content-wrapper"
                          style="padding:20px 20px 10px;border-radius: 30px;margin:20px 0;background-color: #f8f8ff">
                         <div class="confirm fail text-center">
-                            <h3 style="color:#FF6262">{{trans('Payment Failed')}}!</h3>
-                            @if ($errors->any())
-                                <div class="alert alert-danger">
-                                    <ul>
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            @endif
+                            <h3 style="color:#FF6262">{{trans('Payment Success')}}!</h3>
+                            <h2>{{$payment->id}}</h2>
                         </div>
                     </div>
                 </div>
@@ -31,7 +23,7 @@
     <div class="clearfix"></div>
     <div style="text-align: center">
         <a class="btn btn-success"
-           href="{{ url('payment/failed')}}">{{trans('finish')}}</a>
+           href="{{ url('payment/success')}}">{{trans('finish')}}</a>
     </div>
     <div class="clearfix"></div>
 @endsection 
